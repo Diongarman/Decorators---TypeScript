698dd3a3d904c737be375fa2788bba2f45cc9bc5
Dion Garman <diongarman@gmail.com>
2020-04-20 18:01:42 +0100

    Multiple Decorators - order of execution

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,8 @@ Decorator execution precedes a class constructor/instance
 Decorators Factories -
 Can customise decorators by giving them state through closures. Their scoped variables are accessible
 
-
+Bottom up Order of Ops - 
+Sequences of decorators are executed in a 'bottom up' fashion (Kind of, see execution pattern below)
 
 Decorators Arguments
 
@@ -16,33 +17,37 @@ Decorators Arguments
 */
 
 function Logger(logString: string) {
-  console.log("From Decorator Factory to customise decorator 'composed' with:");
+  console.log("From Factory 'Logger':");
   console.log(logString);
   return function (constructor: Function) {
     console.log(
-      'From Decorator:\n\nLogging what I got from factory..\n\n' + logString
+      "Decorator from 'Logger':\n\nLogging what I got from factory..\n\n" +
+        logString
     );
-    console.log('From Decorator: \n\n', constructor);
+    console.log("Decorator from 'Logger': \n\n", constructor);
   };
 }
 
 function WithTemplate(template: string, hookId: string) {
-  //'_' parameter signifies that argument is passed in but not utilised
-  return function (_: Function) {
+  console.log("From Factory 'WithTemplate':");
+  return function (constructor: any) {
+    console.log("Decorator from 'WithTemplate'");
     let hookEl = document.getElementById(hookId);
+    let p = new constructor();
 
     if (hookEl) {
       hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
     }
   };
 }
 
-// @Logger('LOGGING - PERSON')
+@Logger('LOGGING - PERSON')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Dion';
   constructor() {
-    console.log('From Class Instance:\n\nCreating Person...');
+    console.log('From Class Instance!');
   }
 }
 
