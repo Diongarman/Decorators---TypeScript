8c7404b6280fcfae64b8996fc9012eba6077d0a9
Dion Garman <diongarman@gmail.com>
2020-04-20 17:08:34 +0100

    Decorator Factories - state variables passed to decorator via closures

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,22 @@ Decorators Arguments
 
 */
 
-function Logger(constructor: Function) {
-  console.log('Logging..');
-  console.log(constructor);
+function Logger(logString: string) {
+  console.log("From Decorator Factory to customise decorator 'composed' with:");
+  console.log(logString);
+  return function (constructor: Function) {
+    console.log(
+      'From Decorator:\n\nLogging what I got from factory..\n\n' + logString
+    );
+    console.log('From Decorator: \n\n', constructor);
+  };
 }
 
-@Logger()
+@Logger('LOGGING - PERSON')
 class Person {
   name = 'Dion';
   constructor() {
-    console.log('Creating Person...');
+    console.log('From Class Instance:\n\nCreating Person...');
   }
 }
 
